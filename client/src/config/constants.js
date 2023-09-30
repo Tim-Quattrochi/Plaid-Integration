9fe847ddd5eeeae6bcd71097db8d576806fcb6eb
Tim Quattrochi <timq82@gmail.com>
2023-09-30 02:33:06 -0400

    updated constants

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,3 +1,13 @@
 export const API_URL = import.meta.env.VITE_BASE_API || "/api";
-export const APP_NAME = import.meta.env.VITE_APP_NAME || "Mern App starter";
-
+export const APP_NAME =
+  import.meta.env.VITE_APP_NAME || "Plaid-Integration";
+export const VITE_FIRE_API_KEY = import.meta.env.VITE_FIRE_API_KEY;
+export const VITE_AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+export const VITE_PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const VITE_STORAGE_BUCKET = import.meta.env
+  .VITE_STORAGE_BUCKET;
+export const VITE_MESSAGING_SENDER_ID = import.meta.env
+  .VITE_MESSAGING_SENDER_ID;
+export const VITE_FIRE_APP_ID = import.meta.env.VITE_FIRE_APP_ID;
+export const VITE_FIRE_MEASURE_ID = import.meta.env
+  .VITE_FIRE_MEASURE_ID;
