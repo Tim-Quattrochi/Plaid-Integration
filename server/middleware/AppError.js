f3e7f1c9370ae9c42c9e578716758d2d1aa158ae
Tim Quattrochi <timq82@gmail.com>
2023-09-30 00:14:02 -0400

    added custom error for firebase user exists

--- server/middleware/AppError.js
+++ server/middleware/AppError.js
@@ -6,8 +6,9 @@ const AppError = (err, req, res, next) => {
     return res.status(401).json({ error: "Token expired." });
   } else if (err.name === "ValidationError") {
     return res.status(400).json({ error: err.message });
+  } else if (err.code === "auth/email-already-exists") {
+    return res.status(400).json({ error: err.message });
   }
-
   logEvents(
     `${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,
     "errLog.log"
