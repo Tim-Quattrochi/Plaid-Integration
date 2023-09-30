ead9a3eeccb1711440b26968492738e567d34086
Tim Quattrochi <timq82@gmail.com>
2023-09-30 02:33:28 -0400

    created firebase config

---
+++ client/src/config/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import {
+  VITE_AUTH_DOMAIN,
+  VITE_FIRE_API_KEY,
+  VITE_FIRE_MEASURE_ID,
+  VITE_FIRE_APP_ID,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+} from "./constants";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: VITE_FIRE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_FIRE_APP_ID,
+  measurementId: VITE_FIRE_MEASURE_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export { auth };
