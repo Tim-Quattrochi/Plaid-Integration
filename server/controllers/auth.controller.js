fd1fce267d4676afdfefe654a11f8d5fa4db4c8b
Tim Quattrochi <timq82@gmail.com>
2023-10-01 00:33:12 -0400

    auth controller not being used server side since firebase auth is client

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,84 +3,103 @@ const {
   createRefreshToken,
 } = require("../config/createToken");
 const createCookie = require("../config/createCookie");
-//import any user model is needed.
+const { db, auth } = require("../config/db");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const {
   REFRESH_TOKEN_SECRET,
   APP_NAME,
 } = require("../config/constants");
 const CustomError = require("../utils/errorClass");
 
+//this file is currently not being used because user signup/login is client side.
+
 const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    //find the user
+    const user = await auth
+      .createUser({
+        email: email,
+        password: password,
+        refreshToken: null,
+        disabled: false,
+      })
 
-    if (!user) {
-      return next(new CustomError("Check Credentials.", 400));
-    }
+      .then((userRecord) => {
+        console.log("Successfully created new user:", userRecord.uid);
+        const payload = {
+          id: userRecord.uid,
+          email: userRecord.email,
+        };
+        const accessToken = createAccessToken(payload);
 
-    let passCompare; //compare password
-
-    if (!passCompare) {
-      return next(new CustomError("Invalid Credentials.", 401));
-    }
-
-    const payload = { id: user._id, email: user.email };
-
-    const accessToken = createAccessToken(payload);
-
-    const refreshToken = createRefreshToken(payload);
-    createCookie(refreshToken, res);
-
-    user.refreshToken = refreshToken;
-
-    //save the user to the db
-
-    // user = user.toJSON();
-    // delete user.refreshToken;
-    // delete user.password;
-
-    return res
-      .status(200)
-      .json({ ...user, accessToken: accessToken });
+        return res
+          .status(200)
+          .json({ ...userRecord, accessToken: accessToken });
+      });
+    auth.s;
   } catch (error) {
-    console.log(error);
     next(error);
   }
 };
 
+async function createOrRetrieveUser(uid, userData) {
+  const userRef = db.collection("users").doc(uid);
+
+  const userDock = await userRef.get();
+
+  if (!userDock.exists) {
+    await userRef.set(userData);
+  }
+  return userRef;
+}
+
 const signup = async (req, res, next) => {
   const { name, email, password, confirmPassword } = req.body;
+  if (password !== confirmPassword) {
+    return next(new CustomError("Invalid email or password.", 400));
+  }
 
   try {
-    if (password !== confirmPassword) {
-      return next(new CustomError("Invalid email or password.", 400));
-    }
-    let user; //find the user in the db.
+    const user = await auth
+      .createUser({
+        email: email,
+        password: password,
+        refreshToken: null,
+        disabled: false,
+      })
+      .then(async (userRecord) => {
+        console.log("Successfully created new user:", userRecord);
+        await functions.auth.user().onCreate((user) => {
+          user.displayName = name;
+          console.log(user);
+        });
 
-    if (user) {
-      return next(new CustomError("User already exists.", 400));
-    }
+        const payload = {
+          id: userRecord.uid,
+          name: userRecord.name,
+          email: userRecord.email,
+        };
+        const refreshToken = createRefreshToken(payload);
+        const userData = {
+          uid: userRecord.uid,
+          name: name,
+          email: email,
+          refreshToken: refreshToken,
+        };
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+        createCookie(refreshToken, res);
+        const userRef = await createOrRetrieveUser(
+          userRecord.uid,
+          userData
+        );
 
-    let newUser = {
-      /*create the new user*/
-    };
+        const accessToken = createAccessToken(payload);
 
-    const payload = { id: newUser._id, email: newUser.email };
-
-    const accessToken = createAccessToken(payload);
-
-    newUser = newUser.toJSON();
-    delete newUser.password;
-
-    newUser = { ...newUser, accessToken };
-
-    res.status(201).json(newUser);
+        return res
+          .status(200)
+          .json({ payload, accessToken: accessToken });
+      });
   } catch (error) {
     console.log(error);
     next(error);
@@ -149,6 +168,7 @@ const forgotPassword = (req, res, next) => {};
  * @returns {Object} returns a list of numbers
  */
 const list = async (req, res, next) => {
+  console.log(req.user);
   try {
     res.status(200).json({ list: [1, 2, 3, 4, 5] });
   } catch (error) {
