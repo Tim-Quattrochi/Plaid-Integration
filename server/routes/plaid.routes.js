0e2254e6b159526415178b61cc8a6e54684f6820
Tim Quattrochi <timq82@gmail.com>
2023-09-30 22:43:58 -0400

    routes for plaid

---
+++ server/routes/plaid.routes.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const VerifyJWT = require("../middleware/VerifyJWT");
+const {
+  getPlaidLinkToken,
+  exchangePlaidToken,
+  getBalance,
+} = require("../controllers/link.controller");
+
+const plaidRouter = express.Router();
+
+/**
+ * @method {get}
+ * @protected
+ * @{url-endpoint} api/plaid/create_link_token
+ */
+plaidRouter.get("/create_link_token", VerifyJWT, getPlaidLinkToken);
+/**
+ * @method {post}
+ * @protected
+ * @{url-endpoint} api/plaid/exchange_public_token
+ */
+plaidRouter.post(
+  "/exchange_public_token",
+  VerifyJWT,
+  exchangePlaidToken
+);
+/**
+ * @method {get}
+ * @protected
+ * @{url-endpoint} api/plaid/balance
+ */
+plaidRouter.get("/balance", VerifyJWT, getBalance);
+
+module.exports = plaidRouter;
