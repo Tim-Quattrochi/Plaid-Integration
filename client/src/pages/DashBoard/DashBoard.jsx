6938ccd0fe0e0aecf639805cb52d01d4e98a3b99
Tim Quattrochi <timq82@gmail.com>
2023-10-01 00:24:49 -0400

    plaid link on dashboard to connect sandbox account

--- client/src/pages/DashBoard/DashBoard.jsx
+++ client/src/pages/DashBoard/DashBoard.jsx
@@ -1,47 +1,99 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import useAuthContext from "../../hooks/useAuthContext";
 import LoadingSpinner from "../../components/Loading/LoadingSpinner";
+import { usePlaidLink } from "react-plaid-link";
 import "./dashBoard.css";
 
 const DashBoard = () => {
-  const [list, setList] = useState("");
   const axiosPrivate = useAxiosPrivate();
   const { authState } = useAuthContext();
   const [loading, setLoading] = useState(false);
+  const [token, setToken] = useState(null);
+  const [data, setData] = useState(null);
 
   const {
     user: { name },
   } = authState;
-  /**
-   * @description Right now this axios call is just a test
-   * to test protected endpoints
-   */
 
-  useEffect(() => {
+  const onSuccess = useCallback(async (publicToken) => {
     setLoading(true);
-    axiosPrivate
-      .get("/auth/list")
-      .then((res) => {
-        const { data } = res;
-
-        const { list } = data;
-        setList(list);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log(err);
-        setLoading(false);
-      });
+    await axiosPrivate.post("/plaid/exchange_public_token", {
+      public_token: publicToken,
+    });
+    await getBalance();
   }, []);
 
+  // Creates a Link token
+  const createLinkToken = useCallback(async () => {
+    // For OAuth, use previously generated Link token
+    if (window.location.href.includes("?oauth_state_id=")) {
+      const linkToken = localStorage.getItem("link_token");
+      setToken(linkToken);
+    } else {
+      const response = await axiosPrivate.get(
+        "/plaid/create_link_token",
+        {}
+      );
+      const linkToken = response.data.link_token;
+      setToken(linkToken);
+      localStorage.setItem("link_token", data.link_token);
+    }
+  }, [setToken]);
+
+  // Fetch balance data
+  const getBalance = useCallback(async () => {
+    setLoading(true);
+    const response = await axiosPrivate.get("/plaid/balance", {});
+
+    const { data } = response;
+    setData(data);
+    setLoading(false);
+  }, [setData, setLoading]);
+
+  let isOauth = false;
+
+  const config = {
+    token,
+    onSuccess,
+  };
+
+  // For OAuth, configure the received redirect URI
+  if (window.location.href.includes("?oauth_state_id=")) {
+    config.receivedRedirectUri = window.location.href;
+    isOauth = true;
+  }
+  const { open, ready } = usePlaidLink(config);
+
+  useEffect(() => {
+    if (token == null) {
+      createLinkToken();
+    }
+    if (isOauth && ready) {
+      open();
+    }
+  }, [token, isOauth, ready, open]);
+
   if (loading) return <LoadingSpinner />;
+
   return (
     <div className="dash-container">
       <h1>Dashboard</h1>
       <span className="greeting">Hello, {name}</span>
 
-      <span>{list} </span>
+      <div>
+        <button onClick={() => open()} disabled={!ready}>
+          <strong>Link account</strong>
+        </button>
+
+        {!loading &&
+          data != null &&
+          Object.entries(data).map((entry, i) => (
+            <pre key={i}>
+              <code>{JSON.stringify(entry[1], null, 2)}</code>
+            </pre>
+          ))}
+      </div>
     </div>
   );
 };
