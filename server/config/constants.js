6fe8bf58ece452331a3de28cabcb73e3dead9e52
Tim Quattrochi <timq82@gmail.com>
2023-09-30 23:32:47 -0400

    added secret constants

--- server/config/constants.js
+++ server/config/constants.js
@@ -7,6 +7,9 @@ const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 const ACCESS_EXPIRES_IN = process.env.ACCESS_EXPIRES_IN || "1h";
 const REFRESH_EXPIRES_IN = process.env.REFRESH_EXPIRES_IN || "10d";
 const APP_NAME = process.env.APP_NAME || "Plaid Integration";
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID;
+const PLAID_SECRET = process.env.PLAID_SECRET;
+const PLAID_ENV = process.env.PLAID_ENV;
 
 module.exports = {
   PORT,
@@ -18,4 +21,7 @@ module.exports = {
   REFRESH_EXPIRES_IN,
   REFRESH_TOKEN_SECRET,
   APP_NAME,
+  PLAID_CLIENT_ID,
+  PLAID_ENV,
+  PLAID_SECRET,
 };
